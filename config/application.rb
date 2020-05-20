db561d82d07f90b86e17cbcf9c37af5ad797d7b3
lies046 <skytree.skysky@gmail.com>
2020-05-20 05:31:14 +0000

    Modified ConfigApplication and Add BlockedHosts

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,14 @@ module Baukis2
     config.time_zone = "Tokyo"
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_spec false
+    end
   end
 end

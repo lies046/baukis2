05e135fc5c6f9fd419aeb71341a373f0c05f70f2
lies046 <skytree.skysky@gmail.com>
2020-05-21 02:15:01 +0000

    Add ErrorHandlers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,7 @@ class ApplicationController < ActionController::Base
 
   class IpAddressRejected < ActionController::ActionControllerError;end
 
-  rescue_from StandardError, with: :rescue500
-  rescue_from Forbidden, with: :rescue403
-  rescue_from IpAddressRejected, with: :rescue403
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+  include ErrorHandlers if Rails.env.production?
 
   private
   
@@ -18,18 +15,5 @@ class ApplicationController < ActionController::Base
       else
         "customer"
       end
-    end
-
-    def rescue500(e)
-      render "errors/internal_server_error", status: 500
-    end
-
-    def rescue403(e)
-      @exception = e
-      render "errors/forbidden", status: 403
-    end
-
-    def rescue404(e)
-      render "errors/not_found", status: 404
     end
 end

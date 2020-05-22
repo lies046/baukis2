9493d940946d2d017cf5dcab42523f1ecdc7ff5f
lies046 <skytree.skysky@gmail.com>
2020-05-22 01:04:03 +0000

    Add FactoryBot

--- app/services/staff/authenticator.rb
+++ app/services/staff/authenticator.rb
@@ -3,12 +3,12 @@ class Staff::Authenticator
     @staff_member = staff_member
   end
 
-  def authenticate(new_password)
+  def authenticate(raw_password)
     @staff_member && 
     !@staff_member.suspended? && 
     @staff_member.hashed_password &&
     @staff_member.start_date <= Date.today &&
-    (@staff_memebr.end_date.nil? || @staff_member.end_date > Date.today) && 
+    (@staff_member.end_date.nil? || @staff_member.end_date > Date.today) && 
     BCrypt::Password.new(@staff_member.hashed_password) == raw_password
   end
 end

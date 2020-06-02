87c6f00668dd34c8735469f49f27e37b8259dea7
lies046 <skytree.skysky@gmail.com>
2020-06-02 13:19:54 +0000

    Modified StaffMemberModel

--- app/models/concerns/string_normalizer.rb
+++ app/models/concerns/string_normalizer.rb
@@ -4,7 +4,7 @@ module StringNormalizer
   extend ActiveSupport::Concern
 
   def normalize_as_email(text)
-    NKF.nkf("-W -w Z1", text).strip if text
+    NKF.nkf("-W -w -Z1", text).strip if text
   end
 
   def normalize_as_name(text)
--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -38,3 +38,4 @@ class StaffMember < ApplicationRecord
     !suspended? && start_date <= Date.today && (end_date.nil? || end_date > Date.today)
   end
 end
+

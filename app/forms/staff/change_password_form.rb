10ec6d50cdd23aeb583b2ace2499e8c79d50c562
lies046 <skytree.skysky@gmail.com>
2020-06-03 12:41:44 +0000

    Add PasswordsController

---
+++ app/forms/staff/change_password_form.rb
@@ -0,0 +1,10 @@
+class Staff:ChangePasswordForm
+  include ActiveModel::ActiveModel
+
+  attr_accessor :object, :current_password, :new_password, :new_password_confirmation
+
+  def save
+    object.password = new_password
+    object.save!
+  end
+end

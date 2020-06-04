3ab93effa42b37106e52bd6db24caa7cb40f698f
lies046 <skytree.skysky@gmail.com>
2020-06-04 11:52:23 +0000

    Modified ViewsAdminStaffEvents

--- app/lib/html_builder.rb
+++ app/lib/html_builder.rb
@@ -1,14 +1,13 @@
-module HtmlBuider
+module HtmlBuilder
   def markup(tag_name = nil, options = {})
     root = Nokogiri::HTML::DocumentFragment.parse("")
     Nokogiri::HTML::Builder.with(root) do |doc|
       if tag_name
-        doc.method_missing(tag_name, options)
-        yield(doc)
+        doc.method_missing(tag_name, options) do
+          yield(doc)
         end
       else
         yield(doc)
-        end
       end
     end
     root.to_html.html_safe
